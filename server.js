d623fb485e7bd130ad05044451a2c1b7227a9f23
cgathellier <clement.gathellier.dev@gmail.com>
2020-09-08 12:13:12 +0200

    configuration du serveur, connection DB, config routes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
+connectDB();
+
 app.get('/', (req, res) => res.send('Server is running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/admin', require('./routes/api/admin'));
 
 const PORT = process.env.PORT || 5000;
 
